2065a082997b9aa89c4cd15b434e18f1fa3ecda0
Arun <arunbana612@gmail.com>
2025-08-11 17:49:24 +0530

    add in axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatapp-nuhx.vercel.app" : "/api",
-  withCredentials: true,
+  baseURL:
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000" // your local backend when developing
+    : "https://chatapp-nuhx.vercel.app", // backend URL on production
+withCredentials: true,
+
 });
