fd7f1eec3ef4f928a5676fdcd877ead15f44d073
Arun <arunbana612@gmail.com>
2025-08-11 18:01:19 +0530

    changes in axiso

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
   import.meta.env.MODE === "development"
     ? "http://localhost:5000" // your local backend when developing
-    : "https://chatapp-nuhx.vercel.app", // backend URL on production
+    : "https://chatapp-nuhx.vercel.app/api", // backend URL on production
 withCredentials: true,
 
 });
